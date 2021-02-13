d8512498a7262a8858042cff33cedd11e53f30d3
Ignacio <igrasso@itba.edu.ar>
2021-02-13 20:43:18 -0300

    http_provider done. GetAndDecode function

--- UpstreetFlutterCodeChallenge-main/packages/http_provider/lib/failures/failures.dart
+++ UpstreetFlutterCodeChallenge-main/packages/http_provider/lib/failures/failures.dart
@@ -1,7 +1,11 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'failures.freezed.dart';
+
 @freezed
 abstract class Failures with _$Failures {
+  const factory Failures.defaultFailure() = DefaultFailure;
+
   const factory Failures.callFailure(int statusCode) = CallFailure;
 
   const factory Failures.jsonDecodingFailure(String source) =
--- UpstreetFlutterCodeChallenge-main/packages/http_provider/lib/implementation/http_provider.dart
+++ UpstreetFlutterCodeChallenge-main/packages/http_provider/lib/implementation/http_provider.dart
@@ -1,7 +1,9 @@
-library http_provider;
-
-import 'package:http_provider/failures/failures.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 import 'package:dartz/dartz.dart';
+import 'package:http_provider/entities/response.dart';
+import 'package:http_provider/failures/failures.dart';
 import 'package:http_provider/i_http_provider.dart';
 
 class HttpProvider implements IHttpProvider {
@@ -10,10 +12,47 @@ class HttpProvider implements IHttpProvider {
     String url,
     Map<String, String> headers,
     T Function(Map<String, dynamic> json) fromJson,
-  }) {
-    // TODO: implement getAndDecode
-    throw UnimplementedError();
+  }) async {
+    final response = await _get(headers: headers, url: url);
+    return response.fold(
+      (failure) => left(failure),
+      (response) {
+        Map<String, dynamic> json;
+        try {
+          json = convert.json.decode(response.body) as Map<String, dynamic>;
+        } catch (e) {
+          return left(JsonDecodingFailure(e.toString()));
+        }
+        T t;
+        try {
+          t = fromJson(json);
+        } catch (e) {
+          return left(InvalidJsonFailure(e.toString()));
+        }
+        return right(t);
+      },
+    );
   }
 
-  Future<Either<Failure,>>
+  Future<Either<Failures, Response>> _get({
+    @required String url,
+    @required Map<String, String> headers,
+  }) async =>
+      _call(() => http.get(url, headers: headers));
+
+  Future<Either<Failures, Response>> _call(Future<http.Response> call()) async {
+    Either<Failures, Response> either;
+    try {
+      final response = await call();
+      final statusCode = response.statusCode;
+      if (statusCode >= 200 && statusCode < 300) {
+        either = right(Response(statusCode: statusCode, body: response.body));
+      } else {
+        either = left(CallFailure(statusCode));
+      }
+    } catch (e) {
+      either = left(const DefaultFailure());
+    }
+    return either;
+  }
 }
